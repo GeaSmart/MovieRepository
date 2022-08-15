97cb24205eb26128d2fde0f21aface3626e09878
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-14 20:17:47 -0500

    repository interface and class added

---
+++ MovieRepository/Entity/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRepository.Entity
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+    }
+}
---
+++ MovieRepository/Entity/Repository.cs
@@ -0,0 +1,26 @@
+﻿using MovieRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRepository.Entity
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDBContext context;
+
+        public Repository(ApplicationDBContext context)
+        {
+            this.context = context;
+        }
+        public async Task CreateAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
