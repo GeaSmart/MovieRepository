9cb688363440635f886a24979b09ee00956af016
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-14 19:59:58 -0500

    model created, configuring initial

---
+++ MovieRepository/Models/ApplicationDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieRepository.Models
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
